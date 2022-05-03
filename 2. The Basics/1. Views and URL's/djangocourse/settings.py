be09f3db14070a54c2998b19975e4db371b95694
Rudresh <rudresh.narwal@progcap.com>
2022-05-03 17:02:12 +0530

    project level urls and view

--- 2. The Basics/1. Views and URL's/djangocourse/settings.py
+++ 2. The Basics/1. Views and URL's/djangocourse/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'projects.apps.ProjectsConfig'  // added project app
+    'projects.apps.ProjectsConfig' #  added project app
  ]
 
 MIDDLEWARE = [
