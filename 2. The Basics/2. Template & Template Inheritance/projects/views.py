7f88e5f3b717b7d38108e487f7a128f9b35ddfa4
Rudresh <rudresh.narwal@progcap.com>
2022-06-23 17:49:24 +0530

    template inheritance

---
+++ 2. The Basics/2. Template & Template Inheritance/projects/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse('Here is our projects')
+
+
+def project(request, pk):
+    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
