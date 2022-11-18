d41965ef698c1f18a77ac5a34eb8435688ec4dd1
Rudresh <rudresh.narwal@gmail.com>
2022-11-18 16:46:20 +0530

    Added users app

---
+++ 6. Add More Apps/1. User App/users/models.py
@@ -0,0 +1,26 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import User  # importing default django user model
+
+
+# Create your models here.
+
+class Profile(models.Model):
+    # User will have one to one relationship with userModel
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)#On user deletion, it will delete profile entry as well
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    username = models.CharField(max_length=200, null=True, blank=True)
+    short_intro = models.CharField(max_length=200, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_images = models.ImageField(null=True, blank=True, upload_to='profiles/',
+                                       default='profiles/user-default.png')
+    social_github = models.CharField(max_length=200, null=True, blank=True)
+    social_twitter = models.CharField(max_length=200, null=True, blank=True)
+    social_linkedIn = models.CharField(max_length=200, null=True, blank=True)
+    social_website = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
